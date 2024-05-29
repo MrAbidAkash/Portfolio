3a13b12934c8a09ca6431e3d2867443aeeb67df7
Abid Akash <70755047+MrAbidAkash@users.noreply.github.com>
2024-05-29 11:58:14 +0600

    Update Footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,9 +3,11 @@ import { CiLinkedin } from "react-icons/ci";
 import { FiGithub } from "react-icons/fi";
 
 const Footer = () => {
+    const d = new Date();
+let year = d.getFullYear();
     return (
       <div className="flex flex-wrap gap-10 justify-between items-center">
-        <h2 className="text-xl">Copyright © 2023. All rights are reserved</h2>
+        <h2 className="text-xl">Copyright © {year}. All rights are reserved</h2>
         <div className="flex items-center justify-center space-x-3 ">
           <span className="hover:animate-spin animate-bounce">
             <Link
